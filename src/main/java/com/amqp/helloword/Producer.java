715265fdf65f2548bb7f4f839e08b56f15f7b35d
PNZBEIJINGL <beijing_l@sohu.com>
2019-05-04 15:56:46 +0800

    201905041500

---
+++ src/main/java/com/amqp/helloword/Producer.java
@@ -0,0 +1,36 @@
+package com.amqp.helloword;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 创建消费者Produce
+ */
+public class Producer {
+
+    public final static String QUEUE_NAME = "rabbitMQ.test";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //使用工厂创建1个连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        //创建1个通道，声明要关注的队列
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        //发送消息
+        String message = "Hello world";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println("Producer Send +'" + message + "'");
+
+        //关闭通道和连接
+        channel.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/com/amqp/publishtask/Work2.java
@@ -0,0 +1,54 @@
+package com.amqp.publishtask;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Work2 {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        channel.basicQos(1);
+        System.out.println("Worker1  Waiting for messages");
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+
+                String message = new String(body, "UTF-8");
+                System.out.println("Worker2  Received '" + message + "'");
+                try {
+                    //throw  new Exception();
+                    doWork(message);
+
+                } catch (Exception e) {
+                    channel.abort();
+                } finally {
+                    System.out.println("Worker2 Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        boolean autoAck = false;
+        //消息消费完成确认
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, consumer);
+    }
+
+    private static void doWork(String task) {
+        try {
+            Thread.sleep(1000); // 暂停1秒钟
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
