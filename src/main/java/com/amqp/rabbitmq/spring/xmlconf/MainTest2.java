0f24e56b1b9e798c910cafe7a65cfac5de3fe979
PNZBEIJINGL <yong620yong@sohu.com>
2019-05-07 17:43:30 +0800

    201905071200

--- src/main/java/com/amqp/rabbitmq/spring/xmlconf/MainTest2.java
+++ src/main/java/com/amqp/rabbitmq/spring/xmlconf/MainTest2.java
@@ -2,6 +2,7 @@ package com.amqp.rabbitmq.spring.xmlconf;
 
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 /**
  * 方式3 With XML Configuration
@@ -9,11 +10,21 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class MainTest2 {
 
     public static void main(String[] args) {
-        //With XML Configuration
         ApplicationContext context =
-                new GenericXmlApplicationContext("classpath:/rabbit-context.xml");
+                new ClassPathXmlApplicationContext("classpath:/rabbit-context.xml");
+        //
         AmqpTemplate template = context.getBean(AmqpTemplate.class);
-        template.convertAndSend("myqueue", "foo");
+        //发送消息
+        try {
+            template.convertAndSend("myqueue", "foo");
+            Thread.sleep(1000);// 休眠1秒
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            //容器销毁
+            ((ClassPathXmlApplicationContext) context).destroy();
+
+        }
         String foo = (String) template.receiveAndConvert("myqueue");
     }
 }
